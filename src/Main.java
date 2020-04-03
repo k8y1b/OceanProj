5a2215e25f0ae594d024f250b30b13b1ba740959
dylbrown <dylanmb193@gmail.com>
2020-04-03 09:49:29 -0700

    added main with basic gui started

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("fxml/main.fxml"));
+        Scene scene = new Scene(root, 1920, 1080);
+        stage.setTitle("Ocean Project");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
