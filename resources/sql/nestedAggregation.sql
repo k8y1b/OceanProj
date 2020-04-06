85654e7f4d31308048d6b458af6ef6981385b47a
k8y1b <56333166+k8y1b@users.noreply.github.com>
2020-04-05 21:21:22 -0700

    Update nestedAggregation.sql

--- resources/sql/nestedAggregation.sql
+++ resources/sql/nestedAggregation.sql
@@ -1,3 +1,5 @@
 SELECT I.numBodyParts, AVG(A.speed)
 FROM Invertebrate I JOIN Animal A on I.species = A.species
 GROUP BY I.numBodyParts
+
+--selects the animal with the greatest speed out of each category of "numBodyParts"
